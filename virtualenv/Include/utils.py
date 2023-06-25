e38b3b67e25a8884c179aba08802ffa95c0bd479
Sita Alden <aldensita2@gmail.com>
2023-06-25 15:58:02 +0700

    first commit

---
+++ virtualenv/Include/utils.py
@@ -0,0 +1,14 @@
+import os
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "citimclean-bot-no9n-431f92bb3e47.JSON"
+import dialogflow_v2 as dialogflow
+dialogflow_session_client = dialogflow.SessionsClient()
+PROJECT_ID = "citimclean-bot-no9n"
+def detect_intent_from_text(text, session_id, language_code='en'):
+ session = dialogflow_session_client.session_path(PROJECT_ID, session_id)
+ text_input = dialogflow.types.TextInput(text=text, language_code=language_code)
+ query_input = dialogflow.types.QueryInput(text=text_input)
+ response = dialogflow_session_client.detect_intent(session=session, query_input=query_input)
+ return response.query_result
+def fetch_reply(query,session_id):
+ response = detect_intent_from_text(query, session_id)
+ return response.fulfillment_text
